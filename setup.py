500cbce8c329ca191039a0cf6da8c32167190a09
DefaltSimon <simon.peter.goricar@gmail.com>
2017-10-29 19:47:46 +0100

    Added setup, renamed to __init__

---
+++ setup.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+from setuptools import setup
+
+# with open('requirements.txt') as f:
+#     requirements = f.read().splitlines()
+
+
+setup(name='Everglade',
+      version='0.1.0',
+      description='A Minecraft templater engine',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.5',
+          'Intended Audience :: Developers',
+      ],
+      url='https://github.com/DefaltSimon/everglade',
+      author='DefaltSimon',
+      license='MIT',
+      keywords="defaltsimon everglade minecraft templater speed",
+      packages=['everglade'],
+      zip_safe=False)
